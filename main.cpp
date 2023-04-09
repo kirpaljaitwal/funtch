6e8d4cb95bae170d2516b8b2578b338cf3943896
Kirpal Jaitwal <68701235+kirpaljaitwal@users.noreply.github.com>
2023-04-09 23:27:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,113 @@
+// wxWidgets "Hello World" Program
+// For compilers that support precompilation, includes "wx/wx.h".
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+    #include <wx/textctrl.h>
+#endif
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame();
+private:
+    wxTextCtrl *fn_ipText;
+
+
+
+
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    void fnSetMenuBar();
+    void fnSetStatusBar();
+    void fnSetIpTextEntry();
+};
+enum
+{
+    ID_Hello = 1, ID_CONNECT
+};
+wxIMPLEMENT_APP(MyApp);
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
+
+
+void MyFrame::fnSetMenuBar()
+{
+    wxMenu *menuFile = new wxMenu;
+    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+                     "Help string shown in status bar for this menu item");
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_EXIT);
+    wxMenu *menuHelp = new wxMenu;
+    menuHelp->Append(wxID_ABOUT);
+    wxMenuBar *menuBar = new wxMenuBar;
+    menuBar->Append(menuFile, "&File");
+    menuBar->Append(menuHelp, "&Help");
+    SetMenuBar( menuBar );
+    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
+    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+}
+
+void MyFrame::fnSetStatusBar()
+{
+    CreateStatusBar();
+    SetStatusText("Welcome to wxWidgets!");
+}
+
+void MyFrame::OnExit(wxCommandEvent& event)
+{
+    Close(true);
+}
+void MyFrame::OnAbout(wxCommandEvent& event)
+{
+    wxMessageBox("This is a wxWidgets Hello World example",
+                 "About Hello World", wxOK | wxICON_INFORMATION);
+}
+void MyFrame::OnHello(wxCommandEvent& event)
+{
+    wxLogMessage("Hello world from wxWidgets!");
+}
+
+void MyFrame::fnSetIpTextEntry()
+{
+    wxPanel *panel = new wxPanel(this, wxID_ANY);
+
+
+    wxBoxSizer *vsizer = new wxBoxSizer(wxVERTICAL);
+    wxBoxSizer *hsizer = new wxBoxSizer(wxHORIZONTAL);
+
+    fn_ipText = new wxTextCtrl(panel,wxID_ANY,"192.168.0.1",wxDefaultPosition,wxSize(300,-1),0,wxDefaultValidator,"iptext");
+    wxButton *button = new wxButton(panel, ID_CONNECT, wxT("Connect"),wxPoint(20, 20));
+
+    hsizer->Add(fn_ipText);
+    hsizer->Add(button);
+    vsizer->Add(hsizer);
+
+    panel->SetSizer(vsizer);
+}
+
+
+
+
+MyFrame::MyFrame()
+    : wxFrame(NULL, wxID_ANY, "funtch 1.0")
+{
+    fnSetMenuBar();
+    fnSetStatusBar();
+    fnSetIpTextEntry();
+
+
+}
+
